f953151cd8570944784cfada651e6a4713d12457
saurabh-shete <ss.saurabhshete@gmail.com>
2023-01-26 18:45:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,62 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css'
-import reportWebVitals from './reportWebVitals';
+import PeopleList from './components/PeopleList';
+import AddPersonForm from './components/AddPersonForm';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import './style.css';
 
-function AddPersonForm(props){
-  const [person, setPerson] = useState('');
-
-  function handleChange(e){
-    setPerson(e.target.value);
-  }
-
-  function handleSubmit(e){
-    if(person !== ''){
-      props.handleSubmit(person);
-      setPerson('');
-    }
-    e.preventDefault();
-  }
-
-  return(
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Add new contact' onChange={handleChange} />
-        <button type='submit'>Add</button>
-      </form>
-  )
+const initialState = {
+  contacts: ["James Smith", "Thomas Anderson", "Bruce Wayne"]
 };
 
-function PeopleList(props){
-  const arr = props.data;
-  const listItems = arr.map((val,index)=><li key={index}>{val}</li>)
-  return <ul>{listItems}</ul>
-}
-
-function ContactManager(props){
-  const [contacts, setContacts] = useState(props.data);
-
-  function addPerson(name){
-    setContacts([...contacts,name]);
+// Reducer function
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_PERSON':
+      return {
+        ...state,
+        contacts: [...state.contacts, action.data]
+      }
+    default:
+      return state;
   }
-
-  return(
-    <div>
-      <AddPersonForm handleSubmit={addPerson}/>
-      <PeopleList data={contacts}/>
-    </div>
-  )
 }
 
-const contacts = ['James Smith', 'Thomas Anderson', 'Bruce Wayne'];
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ContactManager data={contacts}/>
-  </React.StrictMode>
+const store = createStore(reducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <AddPersonForm />
+    <PeopleList />
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
